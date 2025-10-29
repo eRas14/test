c39327bb083508e467103060919a22c114eea9d1
Aibek Urazov(Macbook) <ibek-94@mail.ru>
2025-10-29 14:32:40 +0500

    Удалены лишние библиотеки в мейне

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types,F
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from keyboards.menus import main_menu
 from hendlers import hendlers_messages
